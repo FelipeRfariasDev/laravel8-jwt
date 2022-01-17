a536f8c0c58114922025366bbda76e88220ac5b4
felipe <felipe.farias.php@gmail.com>
2022-01-16 22:15:05 -0300

    tratamento Exception return json com status http

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -3,17 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Models\Employee;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
     public function store(Request $request){
+
         $employee = new Employee();
         $employee->name = $request->input('name');
         $employee->email = $request->input('email');
         $employee->password = bcrypt($request->input('password'));
-        $employee->job_title = $request->input('Gerente administrativo');
-        $employee->save();
-        return $employee;
+        $employee->job_title = bcrypt($request->input('Gerente administrativo'));
+
+        try {
+            if($employee->save()){
+                return response()->json([
+                    "status"    =>  true,
+                    "message"   =>  "Inserido com sucesso",
+                    "user"      =>  $employee
+                ], 201);
+            }
+        } catch (QueryException $e){
+            return response()->json($e->getMessage());
+        }
+
     }
 }
--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,37 +3,104 @@
 namespace App\Http\Controllers;
 
 use App\Models\Posts;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
     public function index(){
         $posts = Posts::paginate(15);
-        return $posts;
+        return response()->json([
+            "posts"      =>  $posts
+        ]);
     }
 
     public function store(Request $request){
-        $posts = new Posts();
-        $posts->titulo = $request->input('titulo');
-        $posts->descricao = $request->input('descricao');
-        $posts->save();
-        return $posts;
+        $post = new Posts();
+        $post->titulo = $request->input('titulo');
+        $post->descricao = $request->input('descricao');
+
+        try {
+            if($post->save()){
+                return response()->json([
+                    "error"    =>  false,
+                    "success"    =>  true,
+                    "message"   =>  "Inserido com sucesso",
+                    "post"      =>  $post
+                ], 201);
+            }
+        } catch (QueryException $e){
+            return response()->json([
+                "error"    =>  true,
+                "success"    =>  false,
+                "message"   =>  $e->getMessage()
+            ]);
+        }
     }
 
     public function update(Request $request){
-        $posts = Posts::find($request->input('id'));
-        $posts->titulo = $request->input('titulo');
-        $posts->descricao = $request->input('descricao');
-        $posts->save();
-        return $posts;
+
+        $id = $request->input('id');
+
+        $post = Posts::find($id);
+
+        if(!$post) {
+            return response()->json([
+                "error"    =>  true,
+                "success"    =>  false,
+                "message"   => "id $id não foi encontrado",
+            ], 404);
+        }
+
+        $post->titulo = $request->input('titulo');
+        $post->descricao = $request->input('descricao');
+
+        try {
+            if($post->save()){
+                return response()->json([
+                    "error"    =>  false,
+                    "success"    =>  true,
+                    "message"   =>  "Atualizado com sucesso",
+                    "post"      =>  $post
+                ]);
+            }
+        } catch (QueryException $e){
+            return response()->json([
+                "error"    =>  true,
+                "success"    =>  false,
+                "message"   =>  $e->getMessage()
+            ]);
+        }
     }
 
     public function show($id){
-        return Posts::find($id);
+
+        $posts = Posts::find($id);
+        if(!$posts) {
+            return response()->json([
+                "error"    =>  true,
+                "success"    =>  false,
+                "message"   => "id $id não foi encontrado",
+            ], 404);
+        }
     }
 
     public function destroy($id){
+
         $posts = Posts::find($id);
-        return $posts->delete();
+        if(!$posts) {
+            return response()->json([
+                "error"    =>  true,
+                "success"    =>  false,
+                "message"   => "id $id não foi encontrado",
+            ], 404);
+        }
+        if($posts->delete()){
+            return response()->json([
+                "error"    =>  false,
+                "success"    =>  true,
+                "message" => "Post $id removido com sucesso"
+            ]);
+        }
     }
 }
