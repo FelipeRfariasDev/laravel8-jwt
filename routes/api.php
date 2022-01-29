128a692abed5cf6de4f3f61918ef912d74c6f21d
felipe <felipe.farias.php@gmail.com>
2022-01-29 10:43:30 -0300

    protegendo as rotas da api

--- routes/api.php
+++ routes/api.php
@@ -8,14 +8,23 @@ use App\Http\Controllers\PostController;
 
 Route::post('auth/login', [AuthController::class, 'login'])->name('auth.login');
 Route::post('auth/new/user', [UserController::class, 'store'])->name('auth.new.user');
+
+/*
 Route::get('auth/posts', [PostController::class, 'index'])->name('auth.post.index');
 Route::get('auth/posts/{id}', [PostController::class, 'show'])->name('auth.post.show');
+Route::post('auth/posts', [PostController::class, 'store'])->name('auth.posts.store');
+Route::put('auth/posts', [PostController::class, 'update'])->name('auth.posts.update');
+Route::delete('auth/posts/{id}', [PostController::class, 'destroy'])->name('auth.posts.destroy');
+ */
 
 Route::group([ 'middleware' => 'auth-jwt', 'prefix' => 'auth' ], function () {
-
     Route::post('logout', [AuthController::class, 'logout'])->name('auth.logout');
     Route::post('refresh', [AuthController::class, 'refresh'])->name('auth.refresh');
     Route::post('me', [AuthController::class, 'me'])->name('auth.me');
+
+
+    Route::get('posts', [PostController::class, 'index'])->name('auth.post.index');
+    Route::get('posts/{id}', [PostController::class, 'show'])->name('auth.post.show');
 
     Route::post('posts', [PostController::class, 'store'])->name('auth.posts.store');
     Route::put('posts', [PostController::class, 'update'])->name('auth.posts.update');
